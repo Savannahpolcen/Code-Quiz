ec5b8245dd4738afec556e0e9d8546a4b3f18d35
Savannahpolcen <Savannahpolcen@gmail.com>
2022-02-17 22:01:38 -0500

    code to submit, will need to revisit

--- script.js
+++ script.js
@@ -1,11 +1,123 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+
 function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement ('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 
 }
 
-function highScores() {
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 
 }
 
-function selectAnswer() {
-    
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else 
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        answers: [
+            {text:'<script>', correct: true},
+            {text:'<body>', correct:false },
+            {text:'<meta>', correct:false},
+            {text:'<hotdog>', correct:false}
+
+        ]
+    },
+    {
+        question: 'How can you add a comment to a JavaScript?',
+        answers: [
+            {text:'/*', correct: false},
+            {text:'!---*', correct:false },
+            {text:'//', correct: true},
+            {text:'**!**', correct:false}
+        ]
+
+    },
+    {
+
+    question: 'Which operater is used to assign a value to a variable?',
+    answers: [
+        {text:'()', correct: false},
+        {text:'=>', correct:false },
+        {text:'{}', correct: false},
+        {text:'=', correct:true}
+    ]
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+    answers: [
+        {text:'alert.Hello.World', correct: false},
+        {text:'alert{Hello World}', correct:false },
+        {text:'alert("Hello World");', correct: true},
+        {text:'alert"Hello World"', correct:false}
+    ]
+    }
+
+
+] 
